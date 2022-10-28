68b759ea85383fd757135c275f1dc33bae0540c9
Михаил Постников <mihha2397@mail.ru>
2022-10-28 18:05:46 +0300

    try2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,26 +5,29 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    slug = models.SlugField(unique=True, verbose_name='Ссылка')
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='Содержание')
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='Дата публикации')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
         blank=True,
         null=True,
-        related_name='group'
+        related_name='posts',
+        verbose_name='Группа'
     )
